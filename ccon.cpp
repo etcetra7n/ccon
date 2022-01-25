88bc5a512bc3bf0823bc7ae1c7e6b383e253d027
John Anchery <etcetra7n@gmail.com>
2022-01-25 14:54:22 +0530

    trim trailing whitespace

--- ccon.cpp
+++ ccon.cpp
@@ -3,13 +3,13 @@
 #include <fstream>
 #include <windows.h>
 
-void ErrorExit(const std::string &errMsg){ 
+void ErrorExit(const std::string &errMsg){
     LPVOID lpMsgBuf;
     LPVOID lpDisplayBuf;
     DWORD dw = GetLastError();
-    
+
     FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
         FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL,
@@ -17,11 +17,11 @@ void ErrorExit(const std::string &errMsg){
         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
         (LPTSTR) &lpMsgBuf,
         0, NULL );
-    
-    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, errMsg.size() * sizeof(TCHAR)); 
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, errMsg.size() * sizeof(TCHAR));
     sprintf((LPTSTR)lpDisplayBuf, TEXT(errMsg).c_str());
-    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("ccon.exe"), MB_OK); 
-    
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("ccon.exe"), MB_OK);
+
     LocalFree(lpMsgBuf);
     LocalFree(lpDisplayBuf);
     ExitProcess(dw);
@@ -38,18 +38,18 @@ int main(int argc, char* argv[]){
     if (!EmptyClipboard()){
         ErrorExit("Can't empty the clipboard");
     }
-    
+
     std::ifstream file(argv[1]);
     if (!file.good()){
         ErrorExit("Can't open file");
     }
     std::string content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
     const size_t sz = content.length()+1;
-    
+
     HGLOBAL h = GlobalAlloc(GMEM_MOVEABLE, sz);
     memcpy(GlobalLock(h), content.c_str(), sz);
     GlobalUnlock(h);
-    
+
     if (SetClipboardData(CF_TEXT, h) == NULL){
         ErrorExit("Can't set clipboard");
     }

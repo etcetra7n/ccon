7a4da67e0c540c28bb79acab6cbee96104a2fba7
John Anchery <etcetra7n@gmail.com>
2022-01-29 16:22:35 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,6 @@
 #include <windows.h>
-#include <cstdio>
 #include <string>
-#include <fstream>
-
-void ErrorExit(const std::string &errMsg){
-    LPVOID lpMsgBuf;
-    LPVOID lpDisplayBuf;
-    DWORD dw = GetLastError();
-
-    FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER |
-        FORMAT_MESSAGE_FROM_SYSTEM |
-        FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL,
-        dw,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPTSTR) &lpMsgBuf,
-        0, NULL );
-
-    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, errMsg.size() * sizeof(TCHAR));
-    sprintf((LPTSTR)lpDisplayBuf, TEXT(errMsg).c_str());
-    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("CCON"), MB_OK);
-
-    LocalFree(lpMsgBuf);
-    LocalFree(lpDisplayBuf);
-    ExitProcess(dw);
-    exit(dw);
-}
+#include "ccon.h"
 
 int main(int argc, char **argv){
     if (argc < 2){
